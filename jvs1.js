53fb5b84dfc650a3e8b6927bf9e060f504462af5
Vikramaditya Thakur <singh.vikramsingh.vikram92@gmail.com>
2018-12-04 03:56:15 +0530

    initial commit

---
+++ jvs1.js
@@ -0,0 +1,12 @@
+
+// alert("welcome");
+
+// var deposite = prompt("How much to deposite?");
+// alert("you've deposited "+ deposite);
+// console.log(deposite);
+
+alert("Convert Pound to Kilogram. ");
+var pound = prompt("Pound: ");
+alert("KG: " + pound*0.453592);
+
+console.log("Conversion done!");
