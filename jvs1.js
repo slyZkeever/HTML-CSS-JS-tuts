2a51f83f01c08c2661091c146e3f7aa5bd5ad9d7
Vikramaditya Thakur <singh.vikramsingh.vikram92@gmail.com>
2018-12-07 14:06:07 +0530

    Update jvs1.js

--- jvs1.js
+++ jvs1.js
@@ -1,12 +1,45 @@
 
-// alert("welcome");
+/* alert("welcome");
 
-// var deposite = prompt("How much to deposite?");
-// alert("you've deposited "+ deposite);
-// console.log(deposite);
+ var deposite = prompt("How much to deposite?");
+ alert("you've deposited "+ deposite);
+ console.log(deposite);*/
 
-alert("Convert Pound to Kilogram. ");
+/*alert("Convert Pound to Kilogram. ");
 var pound = prompt("Pound: ");
 alert("KG: " + pound*0.453592);
 
-console.log("Conversion done!");
+console.log("Conversion done!");*/
+
+/*var temp = 50;
+console.log(temp >= 30);
+
+if (temp >= 30)
+{    
+    console.log("smaller then 60");    
+}*/
+
+/*var num = 0; //global variable
+
+for(var i = 0; i< 5; i++)
+{
+    console.log(loop());
+}
+
+
+function loop()  //local variable. doesnt need type declaration 
+{
+    /*if(x == 4)
+        return 0;
+    return x++;
+    return num++;
+}*/
+
+
+var word = "abcdefg";
+
+for(var i = 0; i < word.length; i++)
+{
+    console.log(word[i]);
+}
+
