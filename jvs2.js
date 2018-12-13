0d15b144e1fff8b35e81fcf0ebc70c88d17a148e
Vikramaditya Singh thakur <26195113+slyZkeever@users.noreply.github.com>
2018-12-13 13:18:08 +0530

    initial commit

---
+++ jvs2.js
@@ -0,0 +1,15 @@
+/*function add(arg1, arg2)
+{
+    return arg1+arg2;
+    console.log(arg1+arg2);
+}
+
+console.log(add(1, 2));*/
+
+var name;
+function GetName(name = "frank") //default val of name
+{
+    name = "you";
+    return name;  
+}
+
