4704cf098c0a1b132276cfcc3a01047c59267d67
Vikramaditya Thakur <singh.vikramsingh.vikram92@gmail.com>
2018-12-26 20:38:13 +0530

    jq update

---
+++ jq1.js
@@ -0,0 +1,30 @@
+/*$("li"); //grab an/all element
+
+var prop = $("h1");
+
+prop.css("color", "red"); //change css property
+
+prop.css("background", "blue");
+
+var CSSObj = {
+    "color" : "White",
+    "background" : "black",
+    "border" : "5px solid red"
+}
+
+prop.css(CSSObj) //obj can also be passed
+
+
+var listItems = $('li'); 
+
+listItems.css("color", "blue"); //changing property of every element
+
+listItems.eq(0).css("color", "red"); //changing property of perticular element
+
+listItems.eq(-1).css("color", "orange"); // -1 = last item
+
+$("h1").text("fuck off"); //change text. h1 is not saved
+
+$("h1").html("<b> fuck off </b>"); //using "html" to add tag property.
+ 
+*/
