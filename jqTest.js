30e1918e4251b262f4171251faac5ec7b0eb268c
Vikramaditya Thakur <singh.vikramsingh.vikram92@gmail.com>
2018-12-30 04:26:27 +0530

    jqtest

--- jqTest.js
+++ jqTest.js
@@ -4,3 +4,19 @@ var p2 = prompt("Player 2 name: ");
 
 $("h2").text("its Player 1's turn");
 
+var RedCol = 
+{
+    "background-color": "red",
+    "border": "0px"
+}
+
+var BlueCol = 
+{
+    "background-color" : "blue"
+}
+
+$("table tr").click( function()
+{
+    console.log("clicked");
+    $(this).toggleClass("RedCol");
+});
