d7a2f913d9dc96b56f1a48816a63c47bcc8c46a3
Vikramaditya Thakur <singh.vikramsingh.vikram92@gmail.com>
2019-01-03 02:54:53 +0530

    incomp game

--- jqTest.js
+++ jqTest.js
@@ -1,6 +1,60 @@
-var p1 = prompt("Player 1 name: ");
+//intro
+var p1 = prompt("Player 1 name(blue): ");
+var p1Col = "rgb(86, 151, 255)";
 
-var p2 = prompt("Player 2 name: ");
+var p2 = prompt("Player 2 name(red): ");
+var p2Col = "rgb(237, 45, 73)";
+
+var GameOn = true;
+
+//////////////////////////////////
+
+
+//initilize
+var Table = $("table tr");
+var LstRow = 5; //starting from 0
+var LstCol = 6;
+
+function Reportwin(RowIdx, ColIdx)
+{ 
+    console.log("won!");
+    console.log(RowNum + ", " + ColNum);
+}
+
+function SetColor(RowIdx, ColIdx, Color)
+{
+    return Table.$("tr").eq("RowIdx").find("td").eq("ColIdx").find("button").css("background-color", color);
+}
+
+function GetColor(RowIdx, ColIdx)
+{
+    return Table.$("tr").eq("RowIdx").find("td").eq("ColIdx").find("button").css("background-color");
+}
+
+function ChkButtom(ColIdx)
+{
+    var ChkColor;
+
+    for(var row = LstRow; row > -1; row--)
+    {
+        ChkColor = returnGetColor(row, ColIdx);
+        if(ChkColor === 'rgb(128, 128, 128)')
+            return row;
+    }
+}
+
+function ButtonColorMatch(one, two, three, four)
+{
+    return(one === two && one === three && one === four && one !== 'rgb(128, 128, 128)' && one !== undefined)
+}
+
+
+
+////////////////////////////
+
+
+
+
 
 $("h2").text("its Player 1's turn");
 
@@ -15,8 +69,8 @@ var BlueCol =
     "background-color" : "blue"
 }
 
-$("table tr").click( function()
+$("button").on("click", function()
 {
-    console.log("clicked");
-    $(this).toggleClass("RedCol");
+    // $(this).css("background-color", "red");
+    $(this).css("RedCol");
 });
